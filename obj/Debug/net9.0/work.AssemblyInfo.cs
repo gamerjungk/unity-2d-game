a609dcf8f0695f3b173c7fffdfee6e651c44a624
HyoHune <junghyohune@gmail.com>
2025-03-14 17:12:29 +0900

    test

--- obj/Debug/net9.0/work.AssemblyInfo.cs
+++ obj/Debug/net9.0/work.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("work")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5a608e5e8881d7518956b1711dec2fb6f291c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61135b2eed8155fd7453f8e2ad5fc926e2093e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("work")]
 [assembly: System.Reflection.AssemblyTitleAttribute("work")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
