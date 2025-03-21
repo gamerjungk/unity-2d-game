f3d7f38c345e3365f28da20e73137e95dae9f849
gamerjungk <yogyzogy@naver.com>
2025-03-21 15:08:09 +0900

    CarCollision event

---
+++ Assets/2D_urban_cars/scripts/CarController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float rotationSpeed = 200f;
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float turnInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Vertical");   // ↑↓ 키 입력
+        turnInput = Input.GetAxis("Horizontal"); // ←→ 키 입력
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = transform.right * moveInput * speed;
+        rb.angularVelocity = -turnInput * rotationSpeed;
+    }
+    public void Die()
+    {
+        gameObject.SetActive(false);
+        GameManager gameManager = FindFirstObjectByType<GameManager>();
+        Debug.Log("EndGame start");
+        if (gameManager != null)
+        {
+            gameManager.EndGame();
+        }
+        else
+        {
+            Debug.LogWarning("GameManager를 찾을 수 없습니다.");
+        }
+    }
+}
