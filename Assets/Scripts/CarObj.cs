3e9f77fe8ed4a1a04d41bd3ea0256285ef46f3d1
pjhyeok1119 <pjhyeok1119@naver.com>
2025-05-19 18:38:12 +0900

    기어 기름 추가

--- Assets/Scripts/CarObj.cs
+++ Assets/Scripts/CarObj.cs
@@ -13,19 +13,6 @@ public class CarObj : MonoBehaviour
         float randomDistance = Random.Range(1f,2f);
         rb.MovePosition(rb.position +(Vector2)transform.right * randomDistance);
     }
-    void crash()
-    {
-        Debug.Log("Crash!");
-    }
-
-    void OnCollisionEnter2D(Collision2D other)
-    {  // ✅ 수정 (Collider2D → Collision2D)
-        crash();
-        if(other.gameObject.CompareTag("Player")){
-                Debug.Log("플레이어와 충돌 감지됨! Die() 실행");
-                GameManager.inst.Stop();
-        }
-    }
 }
 
 
